ae720359ffcff27171788247261395eb4643723e
Rodrigo Espinoza <rodrigo.espinoza@me.com>
2021-01-12 00:14:36 -0800

    Remove console

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const { locationDistance } = require('./utils');
 // lambda-like handler function
 module.exports.handler = async event => {
   const { httpMethod, path, headers, queryStringParameters } = event;
-  console.log(event);
   const response = defaultResponse();
   const routes = [
     '/zipcode'
